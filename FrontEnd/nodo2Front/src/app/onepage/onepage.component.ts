adb13993696357ae57c6139cc3c0d3db3d5ee193
Pedro Pacheco <pedropacheco@Pedros-MacBook-Pro.local>
2019-10-17 23:54:19 -0400

    Fichas Cools & AutoScroll

--- FrontEnd/nodo2Front/src/app/onepage/onepage.component.ts
+++ FrontEnd/nodo2Front/src/app/onepage/onepage.component.ts
@@ -33,12 +33,10 @@ export class OnepageComponent implements OnInit, OnChanges {
   };
 
   fichas = [
-    '1/1','1/2','1/3','1/4','1/5','1/6',
-    '2/1','2/2','2/3','2/4','2/5','2/6',
-    '3/1','3/2','3/3','3/4','3/5','3/6',
-    '4/1','4/2','4/3','4/4','4/5','4/6',
-    '5/1','5/2','5/3','5/4','5/5','5/6',
-    '6/1','6/2','6/3','6/4','6/5','6/6'
+    '0:0','1:0','1:1','2:0','2:1','2:2','3:0',
+    '3:1','3:2','3:3','4:0','4:1','4:2','4:3',
+    '4:4','5:0','5:1','5:2','5:3','5:4','5:5',
+    '6:0','6:1','6:2','6:3','6:4','6:5','6:6'
   ]
 
   jugadas = []
@@ -200,11 +198,17 @@ export class OnepageComponent implements OnInit, OnChanges {
     this.obtenerJugada();
     this.obtenerPapa();
     this.obtenerFichas();
+    this.updateScroll();
   }
 
   jugadaExitosa(){
     this.pasarPapa();
     this.obtenerJugada();
+  }
+
+  updateScroll(){
+    var element = document.getElementById("scroll");
+    element.scrollTop = element.scrollHeight;
   }
 
 }
