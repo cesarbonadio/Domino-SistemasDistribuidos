8ac68c2040593f4e425daa5afcaa04185869f268
cesarbonadio <cesarbonadio123@gmail.com>
2019-11-05 18:09:05 -0400

    eliminando nodos extras

--- BackEnd/nodo2/config/config.js
+++
@@ -1,7 +0,0 @@
-
-//PAPACALIENTE profiles
-process.env.PORT = 10002;
-process.env.NAMENODE = "JUGADOR 2";
-
-// next player to send papa
-process.env.NEXT = 10003;
--- BackEnd/nodo4/config/config.js
+++
@@ -1,7 +0,0 @@
-
-//PAPACALIENTE profiles
-process.env.PORT = 10004;
-process.env.NAMENODE = "JUGADOR 4";
-
-// next player to send papa
-process.env.NEXT = 10003;
